256fd00f30f93cb3da3cedb18c47545a1b52a94e
Femi <ologunwafemi@gmail.com>
2020-12-11 02:21:59 +0100

    Updates

---
+++ precache-manifest.0d550d4b28a4dd6dd3668afa69cf2e88.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8f3bb0264804b0723f3708a2639025",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "c93963a773942382d9b9",
+    "url": "/weather-app/static/css/2.875acbb4.chunk.css"
+  },
+  {
+    "revision": "2fbad5a8996de4a205a5",
+    "url": "/weather-app/static/css/main.4ff3fd93.chunk.css"
+  },
+  {
+    "revision": "c93963a773942382d9b9",
+    "url": "/weather-app/static/js/2.38ae88ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.38ae88ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fbad5a8996de4a205a5",
+    "url": "/weather-app/static/js/main.9899d36b.chunk.js"
+  },
+  {
+    "revision": "965d95ca7eba495cb2ea",
+    "url": "/weather-app/static/js/runtime-main.38e1c25c.js"
+  },
+  {
+    "revision": "2a55f3ad4f740792db61b8f92c451bc2",
+    "url": "/weather-app/static/media/clear.2a55f3ad.jpg"
+  },
+  {
+    "revision": "2387f6e3e610d40ab79d187d4a0d0e83",
+    "url": "/weather-app/static/media/cloud.2387f6e3.jpg"
+  },
+  {
+    "revision": "bdceda1e47239e0502e890a151b5c2e6",
+    "url": "/weather-app/static/media/default.bdceda1e.jpg"
+  },
+  {
+    "revision": "fb29628ab65e66f5ca5ad3f635eaff6a",
+    "url": "/weather-app/static/media/drizzle.fb29628a.jpg"
+  },
+  {
+    "revision": "c9902a3e2c82bf1b947a0eeaf8c5588d",
+    "url": "/weather-app/static/media/fog.c9902a3e.jpg"
+  },
+  {
+    "revision": "a3c8611d38caf27e6ac68cecf7382f38",
+    "url": "/weather-app/static/media/rain.a3c8611d.jpg"
+  },
+  {
+    "revision": "38fbb5d94d8502c0f06705b37a8ad352",
+    "url": "/weather-app/static/media/snow.38fbb5d9.jpg"
+  },
+  {
+    "revision": "53e639fa476b564a59e9b3daddfa2538",
+    "url": "/weather-app/static/media/sunny.53e639fa.jpg"
+  },
+  {
+    "revision": "68c53d9fd70e266b96ed7f2a29c90d7c",
+    "url": "/weather-app/static/media/thunderstorm.68c53d9f.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/femi-ologunwa/weather-app.git/precache-manifest.2b3cf65af787f6dca6eedec24200dbf7.js"
+  "/weather-app/precache-manifest.0d550d4b28a4dd6dd3668afa69cf2e88.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/femi-ologunwa/weather-app.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
